c045e6ff9a582aabaf2cf170257e5482c4d63c71
Imran <imranradziii@gmail.com>
2022-08-01 09:36:28 +0100

    alternate between 'o' and 'x' in the tic tac toe grid

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-let Gameboard = (() => {
-  let board = ['x', '', 'x',
-                    'x', '', '',
-                    '', 'x', ''];
+const player = (name, marker) => {
+  return {name, marker};
+}
+
+const player1 = player('player1', 'x');
+const player2 = player('player2', 'o');
+const playerList = [player1, player2];
+
+const gameflow = (() => {
+  // index to let us know which player is
+  // currently in turn
+  let playerIndex = 0;
+  const indexAlter = () => {
+    console.log(playerIndex);
+    playerIndex++;
+    return playerIndex %= 2;
+  }
+  return {indexAlter};
+})();
+
+const Gameboard = (() => {
+  let board = ['', '', '',
+                    '', '', '',
+                    '', '', ''];
   
   // takes an html element (preferably div)
   // and appends 9 blocks of div containing
@@ -14,7 +34,7 @@ let Gameboard = (() => {
       // to refine later
       box.addEventListener('click', () => {
         if (box.textContent === '') {
-          box.textContent = currentPlayerMarker;
+          box.textContent = playerList[gameflow.indexAlter()].marker;
         } else {
           console.log('box has been filled');
         }
@@ -29,4 +49,4 @@ let Gameboard = (() => {
 
 const grid = document.querySelector('.grid');
 Gameboard.display(grid);
-currentPlayerMarker = 'o';
+

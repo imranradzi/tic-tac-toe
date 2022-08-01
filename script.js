0a09129d5c4a87ce155e9d2211cce4d09a246763
Imran <imranradziii@gmail.com>
2022-08-01 08:58:18 +0100

    added functionality to add 'x' or 'o' markers to the boxes

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 let Gameboard = (() => {
-  let gameBoard = ['x', 'o', 'x',
-                    'x', 'o', 'o',
-                    'o', 'x', 'o'];
-  return {gameBoard};
+  let board = ['x', '', 'x',
+                    'x', '', '',
+                    '', 'x', ''];
+  
+  // takes an html element (preferably div)
+  // and appends 9 blocks of div containing
+  // the markers
+  const display = (div) => {
+    for (const marker of board) {
+      let box = document.createElement('div');
+      box.textContent = marker;
+
+      // to refine later
+      box.addEventListener('click', () => {
+        if (box.textContent === '') {
+          box.textContent = currentPlayerMarker;
+        } else {
+          console.log('box has been filled');
+        }
+      })
+      // -----------
+
+      div.appendChild(box);
+    }
+  }
+  return {display};
 })();
 
 const grid = document.querySelector('.grid');
-for (const marker of Gameboard.gameBoard) {
-  let div = document.createElement('div');
-  div.textContent = marker;
-  grid.appendChild(div);
-}
+Gameboard.display(grid);
+currentPlayerMarker = 'o';
